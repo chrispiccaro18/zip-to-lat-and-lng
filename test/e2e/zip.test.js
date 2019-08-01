cb531e7ff70598a576de3c28d56ab40dd8d02286
chrispiccaro <chrispiccaro18@gmail.com>
2019-08-01 15:48:21 -0700

    select only lat and lng and empty obj on invalid z

--- test/e2e/zip.test.js
+++ test/e2e/zip.test.js
@@ -8,11 +8,15 @@ describe('zip routes', () => {
       .get('/api/v1/zip/06820');
 
     expect(res.body).toEqual({
-      _id: expect.any(String),
-      __v: 0,
-      zip: '06820',
       lat: '41.076202',
       lng: '-73.480080'
     });
+  });
+
+  it('returns empty object if zip not found', async() => {
+    const res = await request(app)
+      .get('/api/v1/zip/99999');
+
+    expect(res.body).toEqual({});
   });
 });
